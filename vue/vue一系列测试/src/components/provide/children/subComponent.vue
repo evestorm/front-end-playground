93e6d3442fe06e239fd6b5062dcb5902cb4b62e0
evestorm <1572534835@qq.com>
2019-09-12 23:56:06 +0800

    provide响应式Vue.observable

--- vue/vue一系列测试/src/components/provide/children/subComponent.vue
+++ vue/vue一系列测试/src/components/provide/children/subComponent.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
+  <div :style='"background-color: "+theme.color'>
     <h3>我是子组件</h3>
     <p>我获取到了父组件的：{{msg}}</p>
+    <p>{{theme.color}}</p>
   </div>
 </template>
 
 <script>
   export default {
-    inject: ['msg']
+    // inject: ['msg']
+    inject: {
+      msg: {
+        default: String
+      },
+      theme: {
+        default: Object
+      }
+    },
+    watch: {
+      'theme.color'(newValue) {
+        console.log(newValue)
+      }
+    },
   }
 </script>
 
--- vue/vue一系列测试/src/components/provide/provide.vue
+++ vue/vue一系列测试/src/components/provide/provide.vue
@@ -2,15 +2,35 @@
   <div>
     <h1>provide + inject</h1>
     <sub-component></sub-component>
+    <button @click="changeMsg">更改provide的msg（非响应式，无变化）</button><br>
+    <button @click="changeColor">更改provide的theme.color（响应式，子组件背景色改变）</button>{{theme.color}}
   </div>
 </template>
 
 <script>
 import subComponent from './children/subComponent'
+import Vue from 'vue'
   export default {
+    // provide: {
+    //   msg: '...'
+    // }
     // 依赖注入
-    provide: { // 在父组件上提供一个属性，让其所有子组件都可以获取
-      msg: 'provide中的msg'
+    provide() { // 在父组件上提供一个属性，让其所有子组件都可以获取
+      this.theme = Vue.observable({
+        color: 'red'
+      })
+      return {
+        msg: '(provide中的msg，非响应式)',
+        theme: this.theme
+      }
+    },
+    methods: {
+      changeMsg() {
+        this.msg = '开花'
+      },
+      changeColor() {
+        this.theme.color = this.theme.color === 'red' ? 'lightblue' : 'red'
+      }
     },
     components: {
       subComponent
