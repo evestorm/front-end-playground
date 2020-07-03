e8747d9291b576067af9bc1f35d6d8e705526838
evelance <1572534835@qq.com>
2020-07-03 15:38:18 +0800

    长列表更新

--- vue/virtuallist-2/src/components/VirtualList.vue
+++ vue/virtuallist-2/src/components/VirtualList.vue
@@ -25,10 +25,9 @@ export default {
          type: Array,
          default: () => []
       },
-      //每项高度
-      itemSize: {
-         type: Number,
-         default: 200
+      //预估高度
+      estimatedItemSize: {
+         type: Number
       },
       //容器高度 100px or 50vh
       height: {
@@ -39,7 +38,8 @@ export default {
    computed: {
       //列表总高度
       listHeight() {
-         return this.listData.length * this.itemSize;
+         // return this.listData.length * this.itemSize;
+         return this.positions[this.positions.length - 1].bottom;
       },
       //可显示的列表项数
       visibleCount() {
@@ -57,6 +57,31 @@ export default {
             Math.min(this.end, this.listData.length)
          );
       }
+   },
+   created() {
+      this.initPositions();
+   },
+   updated() {
+      this.$nextTick(function() {
+         let nodes = this.$refs.items;
+         nodes.forEach(node => {
+            let rect = node.getBoundingClientRect();
+            let height = rect.height;
+            let index = +node.id.slice(1);
+            let oldHeight = this.positions[index].height;
+            let dValue = oldHeight - height;
+            //存在差值
+            if (dValue) {
+               this.positions[index].bottom =
+                  this.positions[index].bottom - dValue;
+               this.positions[index].height = height;
+               for (let k = index + 1; k < this.positions.length; k++) {
+                  this.positions[k].top = this.positions[k - 1].bottom;
+                  this.positions[k].bottom = this.positions[k].bottom - dValue;
+               }
+            }
+         });
+      });
    },
    mounted() {
       // 获取屏幕高度
@@ -78,6 +103,15 @@ export default {
       };
    },
    methods: {
+      // 初始化postions，存储每项的rect
+      initPositions() {
+         this.positions = this.listData.map((d, index) => ({
+            index,
+            height: this.estimatedItemSize,
+            top: index * this.estimatedItemSize,
+            bottom: (index + 1) * this.estimatedItemSize
+         }));
+      },
       // 监听可视区域的滚动
       scrollEvent() {
          //当前滚动位置
@@ -94,8 +128,40 @@ export default {
          // e.g.
          // scrollTop 50的时候，startOffset = 50 - (50 % 200) = 50 - 50 = 0 不改变偏移
          // scrollTop 200的时候，startOffset = 200 - (200 % 200) = 200 - 0 = 200 改变偏移，让渲染区向下偏移刚才删掉元素的高度
-         this.startOffset = scrollTop - (scrollTop % this.itemSize);
-         console.log(this.startOffset);
+         // this.startOffset = scrollTop - (scrollTop % this.itemSize);
+         if (this.start >= 1) {
+            this.startOffset = this.positions[this.start - 1].bottom;
+         } else {
+            this.startOffset = 0;
+         }
+         console.log(scrollTop, this.startOffset);
+         console.log(scrollTop % this.itemSize);
+      },
+      //获取列表起始索引
+      getStartIndex(scrollTop = 0) {
+         //二分法查找
+         return this.binarySearch(this.positions, scrollTop);
+      },
+      binarySearch(list, value) {
+         let start = 0;
+         let end = list.length - 1;
+         let tempIndex = null;
+
+         while (start <= end) {
+            let midIndex = parseInt((start + end) / 2);
+            let midValue = list[midIndex].bottom;
+            if (midValue === value) {
+               return midIndex + 1;
+            } else if (midValue < value) {
+               start = midIndex + 1;
+            } else if (midValue > value) {
+               if (tempIndex === null || tempIndex > midIndex) {
+                  tempIndex = midIndex;
+               }
+               end = end - 1;
+            }
+         }
+         return tempIndex;
       }
    }
 };
